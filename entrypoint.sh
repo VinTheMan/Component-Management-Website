95a7be06ee0d8aec38c8394227206993a4e7b164
Pegatron <pega@Pegatrons-MacBook-Pro>
2024-02-05 18:35:14 +0800

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-git remote add target http://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#http://}
+git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
 case "${GITHUB_EVENT_NAME}" in
     push)
